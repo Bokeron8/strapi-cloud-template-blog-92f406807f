656810ca9e6ce86851398c003ef75c813a7b53f0
Bokeron8 <francozambrano88@gmail.com>
2026-04-27 22:03:10 -0300

    nose

--- src/api/experiencia/routes/experiencia.ts
+++ src/api/experiencia/routes/experiencia.ts
@@ -1,3 +1,3 @@
 import { factories } from '@strapi/strapi';
 
-export default factories.createCoreRouter('api::experiencia.experiencia' as any);
+export default factories.createCoreRouter('api::experiencia.experiencia');
